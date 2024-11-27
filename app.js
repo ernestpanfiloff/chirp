210e5a8ff2238590d5f25f88f8d7f44b7d2c4bc5
Ernest. <87975970+ernestpanfiloff@users.noreply.github.com>
2024-11-27 18:52:40 +0000

    liked and rechirp buttons working

--- app.js
+++ app.js
@@ -5,32 +5,72 @@ const chirpingButton = document.querySelector(".chirping-button")
 const chirpingInput = document.querySelector(".chirping-input")
 
 
-
 document.addEventListener("click", function (e) {
     if (e.target.dataset.like) {
-        console.log(e.target.dataset.like)
+        handleLike(e.target.dataset.like)
     } else if (e.target.dataset.rechirp) {
-        console.log(e.target.dataset.rechirp)
+        handleRechirp(e.target.dataset.rechirp)
     } else if (e.target.dataset.comment) {
-        console.log(e.target.dataset.comment)
+        handleComment(e.target.dataset.comment)
     }
 })
 
+function handleLike(chirpId) {
+    for (let chirp of chirpsData) {
+        if (chirpId === chirp.uuid && chirp.isLiked === false) {
+            chirp.isLiked = true
+            chirp.likes++
+            renderFeed()
+        } else if (chirpId === chirp.uuid && chirp.isLiked === true) {
+            chirp.isLiked = false
+            chirp.likes--
+            renderFeed()
+        }
+    }
+}
 
+function handleRechirp(chirpId) {
+    for (let chirp of chirpsData) {
+        if (chirpId === chirp.uuid && chirp.isRechirped === false) {
+            chirp.isRechirped = true
+            chirp.rechirps++
+            renderFeed()
+        } else if (chirpId === chirp.uuid && chirp.isRechirped === true) {
+            chirp.isRechirped = false
+            chirp.rechirps--
+            renderFeed()
+        }
+    }
+}
+
+function handleComment(chirpId) {
+    console.log("Comment for: " + chirpId)
+}
 
 function getFeedHtml() {
     let feedHtml = ``
-
+    
     chirpsData.forEach(function (chirp) {
+        let liked = ""
+        let rechirped = ""
+
+        if (chirp.isLiked) {
+            liked = "primary-color"
+        }
+        
+        if (chirp.isRechirped) {
+            rechirped = "primary-color"
+        }
+
         feedHtml += `<div class="feed-item" id="${chirp.uuid}">
                 <div class="avatar"> <img src="${chirp.profilePic}" alt=""></div>
                 <div class="feed-content">
                     <p class="feed-content-name">${chirp.handle}</p>
                     <p class="feed-content-text">${chirp.chirpText}</p>
                     <div class="feed-actions">
-                        <i class="feed-actions-item fa-regular fa-heart" data-like="${chirp.uuid}"> ${chirp.likes}</i>
-                        <i class="feed-actions-item fa-solid fa-retweet" data-rechirp="${chirp.uuid}"> ${chirp.rechirps}</i>
-                        <i class="feed-actions-item fa-regular fa-comment" data-comment="${chirp.uuid}"> ${chirp.replies.length}</i>
+                        <i class="feed-actions-item like-action fa-solid fa-heart ${liked}" data-like="${chirp.uuid}"> ${chirp.likes}</i>
+                        <i class="feed-actions-item fa-solid fa-retweet ${rechirped}" data-rechirp="${chirp.uuid}"> ${chirp.rechirps}</i>
+                        <i class="feed-actions-item fa-solid fa-comment" data-comment="${chirp.uuid}"> ${chirp.replies.length}</i>
                     </div>
                 </div>
             </div>`
@@ -39,8 +79,9 @@ function getFeedHtml() {
     return feedHtml
 }
 
+
+// Clear feed and render it
 function renderFeed() {
     document.querySelector(".feed").innerHTML = getFeedHtml()
 }
-
 renderFeed()
