f685ebcac79e21812b1b13bd70436d592b933249
Ernest. <87975970+ernestpanfiloff@users.noreply.github.com>
2024-11-27 17:16:14 +0000

    added data attributes to chirps

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 import { chirpsData } from "./data.js";
 
 let feed = document.querySelector(".feed")
+const chirpingButton = document.querySelector(".chirping-button")
+const chirpingInput = document.querySelector(".chirping-input")
 
-feed.innerHTML = `<div class="feed-item">
-                <div class="avatar"> <img src="${chirpsData[0].profilePic}" alt=""></div>
+
+
+document.addEventListener("click", function (e) {
+    if (e.target.dataset.like) {
+        console.log(e.target.dataset.like)
+    } else if (e.target.dataset.rechirp) {
+        console.log(e.target.dataset.rechirp)
+    } else if (e.target.dataset.comment) {
+        console.log(e.target.dataset.comment)
+    }
+})
+
+
+
+function getFeedHtml() {
+    let feedHtml = ``
+
+    chirpsData.forEach(function (chirp) {
+        feedHtml += `<div class="feed-item" id="${chirp.uuid}">
+                <div class="avatar"> <img src="${chirp.profilePic}" alt=""></div>
                 <div class="feed-content">
-                    <p class="feed-content-name">${chirpsData[0].handle}</p>
-                    <p class="feed-content-text">${chirpsData[0].chirpText}</p>
+                    <p class="feed-content-name">${chirp.handle}</p>
+                    <p class="feed-content-text">${chirp.chirpText}</p>
                     <div class="feed-actions">
-                        <i class="fa-solid fa-heart"></i>
-                        <i class="fa-solid fa-retweet"></i>
+                        <i class="feed-actions-item fa-regular fa-heart" data-like="${chirp.uuid}"> ${chirp.likes}</i>
+                        <i class="feed-actions-item fa-solid fa-retweet" data-rechirp="${chirp.uuid}"> ${chirp.rechirps}</i>
+                        <i class="feed-actions-item fa-regular fa-comment" data-comment="${chirp.uuid}"> ${chirp.replies.length}</i>
                     </div>
                 </div>
             </div>`
+    })
 
+    return feedHtml
+}
 
-            
+function renderFeed() {
+    document.querySelector(".feed").innerHTML = getFeedHtml()
+}
+
+renderFeed()
--- data.js
+++ data.js
@@ -5,11 +5,23 @@ export let chirpsData = [
         handle: `@curiousKoala`,
         profilePic: `https://openclipart.org/image/2000px/324734`,
         likes: 27,
-        retweets: 10,
+        rechirps: 10,
         chirpText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. Guaranteed return on investment. HMU DMs open!!`,
         replies: [],
         isLiked: false,
-        isRetweeted: false,
+        isRechirped: false,
         uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },
+    {
+        handle: `@crazyFrog`,
+        profilePic: `https://i1.sndcdn.com/artworks-000020497879-dyrqpz-t1080x1080.jpg`,
+        likes: 15,
+        rechirps: 2,
+        chirpText: `Are you a even coder if you only know HTML?`,
+        replies: [],
+        isLiked: false,
+        isRechirped: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
     }
+
 ]
